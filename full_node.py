bfbd8a4658e317c55887b353d7c4ea4d265bc830
Diego Canez <canezdiego@gmail.com>
2021-07-09 17:23:45 -0500

    New methods

--- full_node.py
+++ full_node.py
@@ -1,18 +1,23 @@
 from flask import Flask
 from flask import render_template
 from flask import Blueprint
+from flask import request
 from lib.p2p import P2P
 from lib.blockchain import Blockchain
+from lib.txpool import TxPool
+from pathlib import Path
 import argparse
 
 app = Flask(__name__)
 
 parser = argparse.ArgumentParser(description='Full node.')
-parser.add_argument('key', type=str, help='Signing key.')
+parser.add_argument('key_path', type=str, help='Signing key path.')
 
 args = parser.parse_args()
+KEY_PATH = Path(args.key_path)
 
 blockchain: Blockchain = Blockchain()
+txpool: TxPool = TxPool()
 
 
 @app.route('/latest-block', methods=['GET'])
@@ -22,7 +27,7 @@ def get_latest_block():
 
 @app.route('/balance', methods=['GET'])
 def get_balance():
-    address = requests.args.get('address')
+    address = request.args.get('address')
     return blockchain.balance(address)
 
 
@@ -38,8 +43,25 @@ def get_blockchain():
 
 @app.route('/utxo-sum', methods=['GET'])
 def get_utxo_sum():
-    address = requests.args.get('address')
-    amount = requests.args.get('amount')
+    address = request.args.get('address')
+    amount = request.args.get('amount')
     return blockchain.utxoset.utxo_sum(address, amount)
 
 
+@app.route('/transaction-pool', methods=['GET', 'PUT'])
+def transaction_pool():
+    if request.method == 'GET':
+        return txpool.all()
+    elif request.method == 'PUT':
+        # TODO: unpickle
+        tx = jsonpickle.decode(request.args.get('transaction'))
+        txpool.add(tx)
+
+
+@app.route('/mine', methods=['POST'])
+def mine():
+    # TODO: query peers to update txpool
+    txchunk = txpool.pop_chunk()
+    block = blockchain.create_block(txchunk)
+    # TODO: broadcast
+    blockchain.add_block(block)
